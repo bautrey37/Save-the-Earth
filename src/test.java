64f2841a9cab83df747119d9d4ec9cbc0b155c43
mdworkin <mattdworkin@hotmail.com>
2013-06-14 21:46:59 -0400

    I think I have it working alright.

--- src/test.java
+++ src/test.java
@@ -7,6 +7,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Brandon is here.");
+		System.out.println("Matt is also here.");
 	}
 
 }
