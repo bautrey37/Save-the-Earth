cee081f99bd1ccfb5e95eee561b7a83758db5755
Brandon Autrey <b.autrey@ieee.org>
2013-07-25 15:33:02 -0400

    Added clicking support for game completion screens to go to main menu. Color changed of health bars.  Reduced the white flashes of backstory screens by preloading them when the game starts.  There is still a slight white flash.

--- src/com/github/cop4331sum13/gui/BackStory.java
+++ src/com/github/cop4331sum13/gui/BackStory.java
@@ -1,7 +1,6 @@
 package com.github.cop4331sum13.gui;
 
 import java.awt.CardLayout;
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -18,17 +17,20 @@ public class BackStory extends JPanel implements MouseListener{
 	private String[] images = {"res/BackStory/BackStory1.jpg","res/BackStory/BackStory2.jpg","res/BackStory/BackStory3.jpg","res/BackStory/BackStory4.jpg","res/BackStory/BackStory5.jpg"};
 	private int currentImage;
 	
-	private BufferedImage background;
+	private BufferedImage background[];
 	
 	public BackStory(JPanel container) {
 		this.container = container;
-		this.setBackground(Color.WHITE);
+		//this.setBackground(Color.WHITE);
 		this.addMouseListener(this);
 		currentImage = 0;
 		
+		background = new BufferedImage[5];
+		
 		//try to get image from file
 		try {
-			background = ImageIO.read(new File(images[currentImage])); 
+			for(int i = 0; i < 5; i++)
+				background[i] = ImageIO.read(new File(images[i])); 
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -43,15 +45,8 @@ public class BackStory extends JPanel implements MouseListener{
 	protected void paintComponent(Graphics g) {
 		if (currentImage < images.length){
 			super.paintComponent(g); //calls paintComponent in JPanel
-		
-			//try to get image from file
-			try {
-				background = ImageIO.read(new File(images[currentImage])); 
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
 			
-			g.drawImage(background, 0, 0, null);
+			g.drawImage(background[currentImage], 0, 0, null);
 		}
 		else{
 			currentImage = 0;
--- src/com/github/cop4331sum13/gui/GameCompleted.java
+++ src/com/github/cop4331sum13/gui/GameCompleted.java
@@ -1,15 +1,17 @@
 package com.github.cop4331sum13.gui;
 
+import java.awt.CardLayout;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class GameCompleted extends JPanel implements ActionListener{
+public class GameCompleted extends JPanel implements MouseListener {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel container;
@@ -25,14 +27,32 @@ public class GameCompleted extends JPanel implements ActionListener{
 		}
 	}
 	
-	public void paintComponent(Graphics g) {
+	@Override
+	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.drawImage(background, 0, 0, null);
 	}
 
-
-	public void actionPerformed(ActionEvent e) {
-		
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		CardLayout cl = (CardLayout)container.getLayout();
+		cl.show(container, "title");
 	}
 
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+	}
+	
 }
